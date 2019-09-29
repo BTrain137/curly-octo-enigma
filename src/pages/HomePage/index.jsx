779339c40edfca92eabcb5ddf89aa1b555238754
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-09-29 16:37:11 -0700

    home page layout

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Homepage.styles.scss";
+import Directory from "../../component/Directory/index";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
