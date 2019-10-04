2be05001975ac46480d03d9bd4de12bf05bf7622
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-10-03 22:15:49 -0700

    Create signin and signup page

---
+++ src/pages/SignInAndSignUpPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignInAndSignUpPage.styles.scss";
+
+import SignIn from "../../components/SignIn";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
