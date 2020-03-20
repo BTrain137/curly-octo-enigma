e96e13744a3c95975bdf07235d340993221ff253
btran <btran@Admins-MacBook-Pro-2.local>
2020-03-20 12:05:40 -0700

    Map redux state to header, to manage toggle

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../CartIcon";
+import CartDropdown from "../CartDropdown";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./Header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <header className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -32,11 +33,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </nav>
+    {hidden ? null : <CartDropdown />}
   </header>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
