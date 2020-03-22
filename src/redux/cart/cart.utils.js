4cad526eddbff34f7af6b2bb40a0daedb0fab505
btran <btran@Admins-MacBook-Pro-2.local>
2020-03-22 13:09:59 -0700

    Created decrement item from cart page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,5 +15,21 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-  return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
 };
