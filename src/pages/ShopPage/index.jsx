cd4c3ec493624dc82f136823199d6383145435cc
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-03-27 09:08:32 -0700

    Convert shop page into stateful component

--- src/pages/ShopPage/index.jsx
+++ src/pages/ShopPage/index.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/CollectionOverview";
 import CollectionPage from "../CollectionPage";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+import { firestore } from "../../firebase/firebase.utils";
+
+class ShopPage extends Component {
+  unsubscribeFromSnapShot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+    collectionRef.onSnapshot(async snapShot => {
+      console.log("snapShot", snapShot);
+    })
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
