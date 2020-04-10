b2e14d5b69bc81159e9496ea4fd072f9026479a3
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-10 13:06:25 -0700

    Signout 4 Take in selectors and start action on them within the component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../CartIcon";
 // import CartDropdown from "../CartDropdown";
 import CartDropdownContainer from "../CartDropdown/CartDropdown.container";
 
 import { selectCartHidden } from "../../redux/cart/cart.selector";
 import { selectCurrentUser } from "../../redux/user/user.select";
+import { signOutStart } from "../../redux/user/user.action";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -16,10 +16,10 @@ import {
   HeaderContainer,
   LogoContainer,
   OptionContainer,
-  OptionLink
+  OptionLink,
 } from "./Header.styles.jsx";
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to="/">
       <Logo className="logo" />
@@ -28,7 +28,7 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={signOutStart}>
           SIGN OUT
         </OptionLink>
       ) : (
@@ -42,7 +42,11 @@ const Header = ({ currentUser, hidden }) => (
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+  hidden: selectCartHidden,
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = (dispatch) => ({
+  signOutStart: () => dispatch(signOutStart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
