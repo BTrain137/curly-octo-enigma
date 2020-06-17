b0db5bd3e93b5b4ef9c566323a8003211b496869
btran <btran@Admins-MacBook-Pro-2.local>
2020-06-17 08:31:03 -0700

    Moved all components into lazy loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import HomePage from "./pages/HomePage";
-import ShopPage from "./pages/ShopPage";
-import CheckoutPage from "./pages/CheckoutPage";
 import Header from "./components/Header";
-import SignInAndSignUpPage from "./pages/SignInAndSignUpPage";
+import Spinner from "./components/Spinner";
 
 import { selectCurrentUser } from "./redux/user/user.select";
 import { checkUserSession } from "./redux/user/user.action";
 
 import "./App.css";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ShopPage = lazy(() => import("./pages/ShopPage"));
+const CheckoutPage = lazy(() => import("./pages/CheckoutPage"));
+const SignInAndSignUpPage = lazy(() => import("./pages/SignInAndSignUpPage"));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -22,16 +24,18 @@ const App = ({ checkUserSession, currentUser }) => {
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route
-          exact
-          path="/signin"
-          render={() =>
-            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-          }
-        />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
+        </Suspense>
       </Switch>
     </div>
   );
